b9b3b05b73730911e852a2ebc1d314262c19b2a2
jhcui1 <jc5614@columbia.edu>
2021-11-09 23:16:53 -0500

    added license to each file

--- postProcessorElastic3D.h
+++ postProcessorElastic3D.h
@@ -1,3 +1,27 @@
+/* ----------------------------------------------------------------------
+   iBEM - inclusion-based Boundary Element Method
+   This software is distributed under the GNU General Public License
+   version 3 or any later version.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+    This file is part of iBEM.
+
+    iBEM is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    iBEM is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with iBEM.  If not, see <https://www.gnu.org/licenses/>.
+
+------------------------------------------------------------------------- */
+
 #pragma once
 #include "postProcessor.h"
 #include "configElastic3D.h"
